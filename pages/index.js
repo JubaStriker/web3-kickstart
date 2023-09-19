2bf685fd5bce69a3e0c143ef7979eb6f72f3bd3f
JubaStriker <jubastriker@gmail.com>
2023-09-19 19:38:43 +0600

    Next installed

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewCampaign = () => {
+    return (
+        <div>
+            <h1>This is campaign page</h1>
+        </div>
+    );
+};
+
+export default NewCampaign;
