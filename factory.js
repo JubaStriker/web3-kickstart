55d393c2a354ec9125eebb4b7cb4a3c0193c31c4
JubaStriker <jubastriker@gmail.com>
2023-09-19 19:43:07 +0600

    Contract instance inititated

--- factory.js
+++ factory.js
@@ -1,2 +1,9 @@
 import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json'
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+    JSON.parse(CampaignFactory.interface),
+    '0x5F179f237589B63D3Ef090Fa0D7456fC12F5aAcb'
+)
+
+export default instance;
